fcdbfbd1aa0d53f91a30c13ee4b867b91d34e401
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-04-13 13:58:43 -0400

    up to date for heroku

--- models/sailing.js
+++ models/sailing.js
@@ -8,6 +8,7 @@ const replySchema = new mongoose.Schema({
 const PostsSchema = new mongoose.Schema({
     title: {type: String, required: true},
     body: { type: String, required: true},
+    image: {type: String},
     reply: [replySchema]
 })
 
